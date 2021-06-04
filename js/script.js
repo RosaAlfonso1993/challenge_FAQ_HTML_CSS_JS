529511b3647e8cac2fe05d80c3622148ba3f4979
RosaAlfonso1993 <rosa.alfonso1993@gmail.com>
2021-06-03 22:30:13 -0300

    mejorada la parte de js, todavia le falta

--- js/script.js
+++ js/script.js
@@ -1,12 +1,27 @@
+let active = false;
+//Solo sirve para abrir una opcion y cerrar esa opcion cuando abra otra opcion. No cierra la misma opcion si clickeas de nuevo
 document.querySelectorAll('.card-sideRight-list-question').forEach((item) => {
-    item.addEventListener('click',(event) => {
-        if(!item.classList.contains("response")){
+    item.addEventListener('click', (event) => {
+        if (!active) {
             item.classList.add("response");
             item.parentNode.classList.toggle("active");
+            active = true
         }
-        else{
-            item.classList.remove("response");
-            item.parentNode.classList.remove("active");//accede al div heredado de si mismo 
+        else {
+            active = false
+            document.querySelectorAll('.card-sideRight-list-question').forEach((itm) => {
+                if (itm.classList.contains("response") && itm.classList === item.classList) {
+                    itm.classList.remove("response");
+                    itm.parentNode.classList.remove("active");//accede al div heredado de si mismo       
+                }
+                if (itm.classList.contains("response") && itm.classList !== item.classList) {
+                    itm.classList.remove("response");
+                    itm.parentNode.classList.remove("active");//accede al div heredado de si mismo 
+                    item.classList.add("response");
+                    item.parentNode.classList.toggle("active");
+                }
+            })
+            
         }
     })
-})
+});
